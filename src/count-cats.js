4fe0e81a1e6342ece820aef11bf6d4ef521698c9
Zhibek <jiko633@gmail.com>
2021-03-05 21:55:38 +0400

    fix: cat count done

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,6 +1,16 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function countCats(/* matrix */) {
-  throw new CustomError('Not implemented');
+module.exports = function countCats(cats) {
+ let ab = cats
+   .flat()
+   .filter((it) => typeof it === "string")
+   .filter((x) => x == "^^")
+   .reduce((acc, rec) => {
+     return {
+       ...acc,
+       [rec]: typeof acc[rec] === "undefined" ? 1 : acc[rec] + 1,
+     };
+   }, {});
+ return Object.values(ab);
   // remove line with error and write your code here
 };
