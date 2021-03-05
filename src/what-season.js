d5b4fcc3ce77975c5d955bb091b786492f6ea2b2
Zhibek <jiko633@gmail.com>
2021-03-05 23:24:20 +0400

    fix:what season done but 1 tryil not passed

--- src/what-season.js
+++ src/what-season.js
@@ -1,6 +1,49 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function getSeason(/* date */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function getSeason(data) {
+  if (isNaN(data.getTime())) {
+    return "Unable to determine the time of year!";
+  } else if (data.length === 0) {
+    return null;
+  } else {
+    let month = data.getMonth();
+    switch (month) {
+      case 0:
+        return "winter";
+        break;
+      case 1:
+        return "winter";
+        break;
+      case 2:
+        return "spring";
+        break;
+      case 3:
+        return "spring";
+        break;
+      case 4:
+        return "spring";
+        break;
+      case 5:
+        return "summer";
+        break;
+      case 6:
+        return "summer";
+        break;
+      case 7:
+        return "summer";
+        break;
+      case 8:
+        return "fall";
+        break;
+      case 9:
+        return "fall";
+        break;
+      case 10:
+        return "fall";
+        break;
+      case 11:
+        return "winter";
+        break;
+    }
+  }
 };
