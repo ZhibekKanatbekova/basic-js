d66f43fbd02e62112e9d40bd91ece43a774cdb3e
Zhibek <jiko633@gmail.com>
2021-03-07 14:20:59 +0400

    fix: solution for Deram team is done full

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,14 +1,19 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function createDreamTeam(members) {
-   let cleanArr = members
-     .filter((it) => typeof it === "string")
-     .map((x) => x.trim().slice(0, 1).toUpperCase()).sort()
-     .join("");
+    if (Array.isArray(members)) {
+      let cleanArr = members
+        .filter((it) => typeof it === "string")
+        .map((x) => x.trim().slice(0, 1).toUpperCase())
+        .sort()
+        .join("");
 
-   if (!cleanArr.length) {
-     return false;
-   } else {
-     return cleanArr;
-   }
+      if (!cleanArr.length) {
+        return false;
+      } else {
+        return cleanArr;
+      }
+    } else {
+      return false;
+    }
 };
