1009aaa5043752453279fb4890d88d444d9ede97
Zhibek <jiko633@gmail.com>
2021-03-07 14:09:18 +0400

    fix: solution for transform array, 1test not passing

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,33 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(bar) {
+ let arr = [...bar];
+ let x = [];
+ arr.map((it) => {
+   switch (it) {
+     case "--double-next":
+       x.push(arr[arr.indexOf(it) + 1]);
+       break;
+     case "--double-prev":
+       if (arr.indexOf(it) === 0) {
+         arr.slice(arr.indexOf(it) - 1);
+       } else {
+         x.push(arr[arr.indexOf(it) - 1]);
+       }
+       break;
+     case "--discard-prev":
+       if (arr.indexOf(it) == 0) {
+         arr.slice(arr.indexOf(it));
+       } else {
+         x.splice(x.indexOf(it), 2);
+       }
+       break;
+     case "--discard-next":
+       arr.splice(arr.indexOf(it), 2);
+       break;
+     default:
+       return x.push(it);
+   }
+ });
+ return x.filter((el) => el != null);
 };
